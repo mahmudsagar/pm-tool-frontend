20b472ae5e5934c424013f87cff3090947de725d
Nahid Salekin <36966165+salekin02@users.noreply.github.com>
2024-09-30 22:48:23 +0600

    Update SidebarMenu.jsx

--- src/layouts/elements/SidebarMenu.jsx
+++ src/layouts/elements/SidebarMenu.jsx
@@ -12,19 +12,16 @@ const sidebarTopLinks = [
     title: "Home",
     href: "/",
     icon: LucideHome,
-    className: "bg-slate-100 dark:bg-transparent dark:border text-slate-500  rounded-lg py-2 px-6",
   },
   {
     title: "Calendar",
     href: "/check",
     icon: LucideCalendar,
-    className: "bg-slate-100 dark:bg-transparent dark:border text-slate-500  rounded-lg py-2 px-6",
   },
   {
     title: "Inbox",
     href: "/form/1",
     icon: LucideInbox,
-    className: "bg-slate-100 dark:bg-transparent dark:border text-slate-500  rounded-lg py-2 px-6",
   },
 ];
 
@@ -40,7 +37,8 @@ export function SidebarMenu({ items, setOpen, className }) {
           <Link
             key={item.title}
             href={item.href}
-            className={cn(item.className, path === item.href ? "text-purple-600 bg-purple-50" : "")}>
+            className={cn("bg-slate-100 dark:bg-transparent dark:border text-slate-500 rounded-lg py-2 px-6",
+              path === item.href ? "text-purple-600 bg-purple-50" : "")}>
             <item.icon />
           </Link>
         ))}
