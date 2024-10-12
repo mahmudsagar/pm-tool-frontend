db8d440677e43f45b57e8bfe1a56b6bbf7df763f
Nahid Salekin <36966165+salekin02@users.noreply.github.com>
2024-10-12 13:40:35 +0600

    Update index.jsx

--- src/components/elements/editor/dynamicInput/index.jsx
+++ src/components/elements/editor/dynamicInput/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Button } from '@/components/ui/button';
-import { CopyPlus, Plus, Trash } from 'lucide-react';
+import { CopyPlus, List, Plus, Trash } from 'lucide-react';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import Modal from '../../modal';
 import { DatePicker } from './datepicker';
@@ -29,20 +29,21 @@ const SelectField = ({ options, ...props }) => {
 
 
 const fieldTypes = [
-  { type: 'input', label: 'Input', component: Input },
-  { type: 'number', label: 'Number', component: InputNumber },
+  { type: 'input', label: 'Input', component: Input, hasOptions: false },
+  { type: 'number', label: 'Number', component: InputNumber, hasOptions: false },
   { type: 'select', label: 'Select', component: SelectField, hasOptions: true },
-  { type: 'date', label: 'Date', component: DatePicker },
-  { type: 'daterange', label: 'Date Range', component: DatePickerWithRange },
+  { type: 'date', label: 'Date', component: DatePicker, hasOptions: false },
+  { type: 'daterange', label: 'Date Range', component: DatePickerWithRange, hasOptions: false },
 ];
 
 const Field = ({ field, onChange }) => {
   const [open, setOpen] = useState(false);
   const [label, setLabel] = useState(field.label);
   const [actionType, setActionType] = useState('edit');
-  const [fieldType, setFieldType] = useState(field.type);
   /** options array for select, radio, checkbox */
   const [options, setOptions] = useState([]);
+  const [showFieldList, setShowFieldList] = useState(false);
+  const [changedField, setChangedField] = useState({});
 
   useEffect(() => {
     if (!field.initialized) {
@@ -54,7 +55,8 @@ const Field = ({ field, onChange }) => {
       <DropdownMenu open={open} onOpenChange={state => {
         setOpen(state);
         if (!state) {
-          onChange({ ...field, type: fieldType, label, actionType, initialized: true, options: options.filter(Boolean) });
+          setShowFieldList(false);
+          onChange({ ...field, label, actionType, initialized: true, options: options.filter(Boolean), ...(changedField ? { ...changedField } : {}) });
         }
       }}>
         <DropdownMenuTrigger asChild>
@@ -62,66 +64,83 @@ const Field = ({ field, onChange }) => {
         </DropdownMenuTrigger>
 
         <DropdownMenuContent >
-          <DropdownMenuLabel>
-            <Input
-              className="h-8"
-              id="field-label-input"
-              placeholder="Enter label"
-              onChange={e => setLabel(e.target.value)}
-              value={label}
-            />
-          </DropdownMenuLabel>
 
-          {field?.hasOptions && <>
+          {!showFieldList && <>
+            <DropdownMenuItem className="items-center justify-between cursor-pointer" onClick={e => {
+              e.preventDefault();
+              setShowFieldList(true);
+            }}>
+              <h5 className="text-sm">Field Type</h5>
+              <List size={12} />
+            </DropdownMenuItem>
             <DropdownMenuSeparator />
-            <DropdownMenuItem className="flex-col gap-2 items-start focus:bg-transparent" onClick={e => { e.preventDefault() }}>
-              <h5 className="text-sm mt-2">Options</h5>
-              <div className="flex flex-col gap-2 items-start p-1 max-h-40 overflow-y-auto better-scrollbar">
-                {options.map((option, index) => (
-                  <div key={index} className="flex gap-1 justify-between items-center">
-                    <Input
-                      onKeyDown={e => e.stopPropagation()}
+          </>}
+
+          {showFieldList ?
+
+            <FieldList handleCreateField={setChangedField} />
+            :
+            <>
+              <DropdownMenuLabel>
+                <Input
+                  className="h-8"
+                  id="field-label-input"
+                  placeholder="Enter label"
+                  onChange={e => setLabel(e.target.value)}
+                  value={label}
+                />
+              </DropdownMenuLabel>
+
+              {field?.hasOptions && <>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="flex-col gap-2 items-start focus:bg-transparent" onClick={e => { e.preventDefault() }}>
+                  <h5 className="text-sm mt-2">Options</h5>
+                  <div className="flex flex-col gap-2 items-start p-1 max-h-40 overflow-y-auto better-scrollbar">
+                    {options.map((option, index) => (
+                      <div key={index} className="flex gap-1 justify-between items-center">
+                        <Input
+                          onKeyDown={e => e.stopPropagation()}
+                          className="h-8"
+                          onChange={e => {
+                            const updatedOptions = [...options];
+                            updatedOptions[index] = e.target.value;
+                            setOptions(updatedOptions);
+                          }}
+                          value={option}
+                        />
+                        <Button variant="ghost" size="sm" className="px-2 py-1" onClick={() => {
+                          const updatedOptions = options.filter((_, i) => i !== index);
+                          setOptions(updatedOptions);
+                        }}>
+                          <Trash size={12} />
+                        </Button>
+                      </div>
+                    ))}
+                    <Button
+                      variant="outline"
+                      onClick={() => setOptions([...options.filter(Boolean), ''])}
                       className="h-8"
-                      onChange={e => {
-                        const updatedOptions = [...options];
-                        updatedOptions[index] = e.target.value;
-                        setOptions(updatedOptions);
-                      }}
-                      value={option}
-                    />
-                    <Button variant="ghost" size="sm" className="px-2 py-1" onClick={() => {
-                      const updatedOptions = options.filter((_, i) => i !== index);
-                      setOptions(updatedOptions);
-                    }}>
-                      <Trash size={12} />
+                    >
+                      Add new
                     </Button>
                   </div>
-                ))}
-                <Button
-                  variant="outline"
-                  onClick={() => setOptions([...options.filter(Boolean), ''])}
-                  className="h-8"
-                >
-                  Add new
-                </Button>
-              </div>
-            </DropdownMenuItem>
-          </>
-          }
-
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem className="cursor-pointer" onClick={() => setActionType('duplicate')}>
-              <div className='flex items-center gap-1'>
-                <CopyPlus size={12} /> Duplicate property
-              </div>
-            </DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer hover:text-red-600" onClick={() => setActionType('delete')}>
-              <div className='flex items-center gap-1'>
-                <Trash size={12} /> Delete property
-              </div>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
+                </DropdownMenuItem>
+              </>
+              }
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem className="cursor-pointer" onClick={() => setActionType('duplicate')}>
+                  <div className='flex items-center gap-1'>
+                    <CopyPlus size={12} /> Duplicate property
+                  </div>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer hover:text-red-600" onClick={() => setActionType('delete')}>
+                  <div className='flex items-center gap-1'>
+                    <Trash size={12} /> Delete property
+                  </div>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </>}
         </DropdownMenuContent>
       </DropdownMenu>
     </td>
@@ -131,15 +150,30 @@ const Field = ({ field, onChange }) => {
   </tr>
 }
 
+const FieldList = ({ handleCreateField }) => {
+  const [searchValue, setSearchValue] = useState('');
+  return <>
+    <DropdownMenuItem className="mb-1.5" onClick={e => e.preventDefault()} asChild>
+      <Input className="py-1 h-8 cursor-auto focus-visible:ring-0" placeholder="Search a property" onKeyDown={e => e.stopPropagation()} onChange={e => setSearchValue(e.target.value)} />
+    </DropdownMenuItem>
+
+    {fieldTypes.filter(field => field.label.toLowerCase().includes(searchValue.toLowerCase()))
+      .map((field, index) => (
+        <DropdownMenuItem key={index} className="cursor-pointer" onClick={() => {
+          handleCreateField(field);
+        }}>
+          {field.label}
+        </DropdownMenuItem>
+      ))}
+  </>
+}
+
 
 const DynamicInput = () => {
   const [fields, setFields] = useState([]); // List of added fields
 
-  const [searchValue, setSearchValue] = useState('');
-
   const handleCreateField = ({ type, ...rest }) => {
     /** store the new field in the list by a random id */
-
     const newField = {
       id: Math.random().toString(10).substring(7),
       component: fieldTypes.find(field => field.type === type).component,
@@ -190,23 +224,11 @@ const DynamicInput = () => {
             Add a property</Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56">
-
-          <DropdownMenuItem className="mb-1.5" onClick={e => e.preventDefault()} asChild>
-            <Input className="py-1 h-8 cursor-auto focus-visible:ring-0" placeholder="Search a property" onKeyDown={e => e.stopPropagation()} onChange={e => setSearchValue(e.target.value)} />
-          </DropdownMenuItem>
-
-          {fieldTypes.filter(field => field.label.toLowerCase().includes(searchValue.toLowerCase()))
-            .map((field, index) => (
-              <DropdownMenuItem key={index} className="cursor-pointer" onClick={() => {
-                handleCreateField(field);
-              }}>
-                {field.label}
-              </DropdownMenuItem>
-            ))}
+          <FieldList handleCreateField={handleCreateField} />
         </DropdownMenuContent>
       </DropdownMenu>
       <Modal />
-    </div>
+    </div >
   );
 };
 
