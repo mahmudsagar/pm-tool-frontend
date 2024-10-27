99de707f355d9b3e47b78748ae518fe84fa0e24b
Nahid Salekin <36966165+salekin02@users.noreply.github.com>
2024-10-27 22:38:16 +0600

    enabled parallel page accessibility

--- src/BetterRouter/Drawer.jsx
+++ src/BetterRouter/Drawer.jsx
@@ -1,11 +1,13 @@
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetHeader } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
 
 const Drawer = ({ children, header = '', contentClassName = '', ...props }) => {
 
 
   return <Sheet {...props}>
-    <SheetContent className={cn("w-[500px] sm:w-[640px] lg:max-w-screen-lg overflow-y-scroll max-h-screen", contentClassName)}>
+    <SheetContent  closeBtn={false} className={cn("w-[600px] sm:w-[740px] lg:max-w-screen-lg overflow-y-scroll max-h-screen", contentClassName)} onInteractOutside={(e) => {
+      e.preventDefault();
+    }}>
       <SheetHeader>
         {header}
       </SheetHeader>
--- src/BetterRouter/ParallelRoute/Page.jsx
+++ src/BetterRouter/ParallelRoute/Page.jsx
@@ -4,13 +4,15 @@ import React from "react";
 import Drawer from "../Drawer";
 import { routes } from "../routes";
 import Header from "@/layouts/elements/header";
-
+import './style.scss';
+import { SheetClose } from "@/components/ui/sheet";
+import { X } from "lucide-react";
 
 const ParallelRoutePage = ({ path, target }) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [open, setOpen] = React.useState(true);
 
-  /** will implement modal later */
+  /** will implement modal later, has some issues with the dialog component */
   const Container = target === '_popup' ? Drawer : Drawer;
 
 
@@ -30,8 +32,10 @@ const ParallelRoutePage = ({ path, target }) => {
   const containerProps = {
     open,
     onOpenChange: onClose,
-    contentClassName: 'p-0',
-    header: <Header showPageTitle={false} />
+    contentClassName: 'p-0 parallel-page-sheet-content',
+    modal: false,
+    closeBtn: false
+    // header: <Header showPageTitle={false} />
   };
 
   /** finding similar routes based on current path */
@@ -44,6 +48,14 @@ const ParallelRoutePage = ({ path, target }) => {
     return '';
   }
   return <Container {...containerProps}>
+    <Header showPageTitle={false} closeBtn={
+      <SheetClose
+        className="sheet-close-btn absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+        <X className="h-4 w-4" />
+        <span className="sr-only">Close</span>
+      </SheetClose>
+    } />
+
     {route.element}
   </Container>
 }
