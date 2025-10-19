6477274b2ec6214559e545c0920a58ec69fa725c
mohammadshariful <mohammadsharifulislam64@gmail.com>
2025-10-20 00:36:04 +0600

    add extra feature of univer sheets

--- src/components/elements/spreadsheet/index.jsx
+++ src/components/elements/spreadsheet/index.jsx
@@ -1,18 +1,49 @@
-import '@univerjs/design/lib/index.css';
-
-import { Univer, LocaleType, UniverInstanceType } from '@univerjs/core';
+import { forwardRef, memo, useEffect, useImperativeHandle, useRef } from 'react'
+import { LocaleType,  Univer, UniverInstanceType } from '@univerjs/core'
+import { UniverDocsPlugin } from '@univerjs/docs'
+import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
+import { UniverFormulaEnginePlugin } from'@univerjs/engine-formula'
+import { UniverRenderEnginePlugin } from '@univerjs/engine-render'
+import { UniverSheetsPlugin } from '@univerjs/sheets'
 import { defaultTheme } from '@univerjs/design';
-import { UniverDocsPlugin } from '@univerjs/docs';
-import { UniverDocsUIPlugin } from '@univerjs/docs-ui';
-import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula';
-import { UniverRenderEnginePlugin } from '@univerjs/engine-render';
-import { UniverSheetsPlugin } from '@univerjs/sheets';
-import { UniverSheetsFormulaPlugin } from '@univerjs/sheets-formula';
-import { UniverSheetsUIPlugin } from '@univerjs/sheets-ui';
-import { UniverUIPlugin } from '@univerjs/ui';
-import { forwardRef, memo, useEffect, useImperativeHandle, useRef } from 'react';
-import './index.css';
+import { UniverSheetsConditionalFormattingUIPlugin } from '@univerjs/sheets-conditional-formatting-ui'
+import { UniverSheetsCrosshairHighlightPlugin } from '@univerjs/sheets-crosshair-highlight'
+import { UniverSheetsDataValidationPlugin } from '@univerjs/sheets-data-validation'
+import { UniverSheetsDataValidationUIPlugin } from '@univerjs/sheets-data-validation-ui'
+import { UniverSheetsFilterUIPlugin } from '@univerjs/sheets-filter-ui'
+import { UniverSheetsFindReplacePlugin } from '@univerjs/sheets-find-replace'
+import { UniverSheetsFormulaUIPlugin } from '@univerjs/sheets-formula-ui'
+import { UniverSheetsHyperLinkUIPlugin } from '@univerjs/sheets-hyper-link-ui'
+import { UniverSheetsNoteUIPlugin } from '@univerjs/sheets-note-ui'
+import { UniverSheetsNumfmtUIPlugin } from '@univerjs/sheets-numfmt-ui'
+import { UniverSheetsSortUIPlugin } from '@univerjs/sheets-sort-ui'
+import { UniverSheetsTableUIPlugin } from '@univerjs/sheets-table-ui'
+import { UniverSheetsThreadCommentUIPlugin } from '@univerjs/sheets-thread-comment-ui'
+import { UniverSheetsUIPlugin } from '@univerjs/sheets-ui'
+import { UniverSheetsZenEditorPlugin } from '@univerjs/sheets-zen-editor'
+import { UniverThreadCommentUIPlugin } from '@univerjs/thread-comment-ui'
+import { UniverUIPlugin } from '@univerjs/ui'
 import { zhCN, enUS } from 'univer:locales'
+import './index.css'
+
+import '@univerjs/design/lib/index.css'
+import '@univerjs/ui/lib/index.css'
+import '@univerjs/docs-ui/lib/index.css'
+import '@univerjs/sheets-ui/lib/index.css'
+import '@univerjs/sheets-formula-ui/lib/index.css'
+import '@univerjs/sheets-numfmt-ui/lib/index.css'
+import '@univerjs/sheets-sort-ui/lib/index.css'
+import '@univerjs/sheets-filter-ui/lib/index.css'
+import '@univerjs/find-replace/lib/index.css'
+import '@univerjs/sheets-conditional-formatting-ui/lib/index.css'
+import '@univerjs/sheets-data-validation-ui/lib/index.css'
+import '@univerjs/sheets-hyper-link-ui/lib/index.css'
+import '@univerjs/sheets-note-ui/lib/index.css'
+import '@univerjs/sheets-table-ui/lib/index.css'
+import '@univerjs/thread-comment-ui/lib/index.css'
+import '@univerjs/sheets-zen-editor/lib/index.css'
+import '@univerjs/sheets-crosshair-highlight/lib/index.css'
+
 
 // eslint-disable-next-line react/display-name
 const UniverSheet = forwardRef(({ data }, ref) => {
@@ -43,8 +74,8 @@ const UniverSheet = forwardRef(({ data }, ref) => {
     univerRef.current = univer;
 
     // core plugins
-    univer.registerPlugin(UniverRenderEnginePlugin);
-    univer.registerPlugin(UniverFormulaEnginePlugin);
+    univer.registerPlugin(UniverRenderEnginePlugin)
+    univer.registerPlugin(UniverFormulaEnginePlugin)
     univer.registerPlugin(UniverUIPlugin, {
       container: containerRef.current,
     });
@@ -55,11 +86,27 @@ const UniverSheet = forwardRef(({ data }, ref) => {
     });
     univer.registerPlugin(UniverDocsUIPlugin);
 
-    // sheet plugins
-    univer.registerPlugin(UniverSheetsPlugin);
-    univer.registerPlugin(UniverSheetsUIPlugin);
-    univer.registerPlugin(UniverSheetsFormulaPlugin);
 
+    // sheet plugins
+
+    univer.registerPlugin(UniverSheetsPlugin)
+    univer.registerPlugin(UniverSheetsUIPlugin)
+    univer.registerPlugin(UniverSheetsFormulaUIPlugin)
+    univer.registerPlugin(UniverSheetsNumfmtUIPlugin)
+
+    univer.registerPlugin(UniverSheetsDataValidationPlugin)
+    univer.registerPlugin(UniverSheetsDataValidationUIPlugin)
+    univer.registerPlugin(UniverSheetsConditionalFormattingUIPlugin)
+    univer.registerPlugin(UniverSheetsFilterUIPlugin)
+    univer.registerPlugin(UniverSheetsSortUIPlugin)
+    univer.registerPlugin(UniverSheetsFindReplacePlugin)
+    univer.registerPlugin(UniverSheetsHyperLinkUIPlugin)
+    univer.registerPlugin(UniverThreadCommentUIPlugin)
+    univer.registerPlugin(UniverSheetsThreadCommentUIPlugin)
+    univer.registerPlugin(UniverSheetsTableUIPlugin)
+    univer.registerPlugin(UniverSheetsNoteUIPlugin)
+    univer.registerPlugin(UniverSheetsCrosshairHighlightPlugin)
+    univer.registerPlugin(UniverSheetsZenEditorPlugin)
     // create workbook instance
     workbookRef.current = univer.createUnit(UniverInstanceType.UNIVER_SHEET, data);
   };
